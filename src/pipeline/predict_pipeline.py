9cecf9a98e391b85978f0756c13e56309cc896e9
Sanskar-Jayale <sanskarjayale2003@gmail.com>
2024-05-02 23:41:10 +0530

    Completed the flask application and predict pipeline

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,54 @@
+import sys
+import pandas as pd
+
+from src.exception import CustomException
+from src.utils import load_object
+
+
+class PredictPipeline:
+    def __name__(self):
+        pass
+
+    def predict(self, features):
+        try:
+            model_path ='artifacts\model.pkl'
+            preprocessor_path ='artifacts\pro   processor.pkl'
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+            data_scaled = preprocessor.transform(features)
+            pred = model.predict(data_scaled)
+            return pred
+        except Exception as e:
+            raise CustomException(e, sys)
+
+class CustomeData:
+    def __init__(self,
+                 gender:str,
+                 race_ethnicity:str,
+                 parental_level_of_education:str,
+                 lunch:str,
+                 test_preparation_course:str,
+                 writing_score:int,
+                 reading_score:int):
+        self.gender = gender
+        self.race_ethnicity = race_ethnicity
+        self.parental_level_of_education = parental_level_of_education
+        self.lunch = lunch
+        self.test_preparation_course = test_preparation_course
+        self.writing_score = writing_score
+        self.reading_score = reading_score
+
+    def Custome_data_to_data_frame(self):
+        try:
+            custome_data_input_dict = {
+                "gender":[self.gender],
+                "race_ethnicity":[self.race_ethnicity],
+                "parental_level_of_education":[self.parental_level_of_education],
+                "lunch":[self.lunch],
+                "test_preparation_course":[self.test_preparation_course],
+                "writing_score":[self.writing_score],
+                "reading_score":[self.reading_score]
+            }
+            return pd.DataFrame(custome_data_input_dict)
+        except Exception as e:
+            raise CustomException(e, sys)
